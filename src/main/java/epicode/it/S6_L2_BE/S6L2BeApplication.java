817c41686b2302e841ff78a60a9822c2cc744d05
andreaDevelope <andrea_develope@outlook.it>
2025-01-08 13:45:21 +0100

    first-commit

---
+++ src/main/java/epicode/it/S6_L2_BE/S6L2BeApplication.java
@@ -0,0 +1,13 @@
+package epicode.it.S6_L2_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S6L2BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S6L2BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/epicode/it/S6_L2_BE/S6L2BeApplicationTests.java
@@ -0,0 +1,13 @@
+package epicode.it.S6_L2_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S6L2BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
